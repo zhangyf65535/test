6b7d7a559f0cdc4fa31724717dd8e77e17fc84f9
zhangyf65535 <403255196@qq.com>
2022-07-07 15:30:40 +0800

    版本5

--- Web/common/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# @Time : 2022/7/5 13:59
-# @Author : zyf
-# @File : __init__.py.py
--- Web/page_objects/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-# @Time : 2022/7/5 13:57
-# @Author : zyf
-# @File : __init__.py.py
